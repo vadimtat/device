a41898a34393f3957ddb1a64311b18a1e57e6cd5
vadimtat <47364514+vadimtat@users.noreply.github.com>
2022-11-04 18:36:04 +0300

    исправляет модалку

--- source/js/main.js
+++ source/js/main.js
@@ -19,111 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
     initModals();
   });
 });
-let footerSection = document.querySelector('.footer-section');
-let footerSectionToggle = document.querySelector('.footer-section__toggle');
-
-footerSection.classList.remove('footer-section--nojs');
-
-footerSectionToggle.addEventListener('click', function () {
-  if (footerSection.classList.contains('footer-section--closed')) {
-    footerSection.classList.remove('footer-section--closed');
-    footerSection.classList.add('footer-section--opened');
-    footerAddress.classList.remove('footer-address--opened');
-    footerAddress.classList.add('footer-address--closed');
-  } else {
-    footerSection.classList.add('footer-section--closed');
-    footerSection.classList.remove('footer-section--opened');
-  }
-});
-let footerAddress = document.querySelector('.footer-address');
-let footerAddressToggle = document.querySelector('.footer-address__toggle');
-
-footerAddress.classList.remove('footer-address--nojs');
-
-footerAddressToggle.addEventListener('click', function () {
-  if (footerAddress.classList.contains('footer-address--closed')) {
-    footerAddress.classList.remove('footer-address--closed');
-    footerAddress.classList.add('footer-address--opened');
-    footerSection.classList.remove('footer-section--opened');
-    footerSection.classList.add('footer-section--closed');
-  } else {
-    footerAddress.classList.add('footer-address--closed');
-    footerAddress.classList.remove('footer-address--opened');
-  }
-});
-let mainModal = document.querySelector('.main-modal');
-let mainnavButton = document.querySelector('.main-nav__button');
-let maimodalButton = document.querySelector('.main-modal__close-button');
-mainnavButton.addEventListener('click', function () {
-  if (mainModal.classList.contains('main-modal--closed')) {
-    mainModal.classList.remove('main-modal--closed');
-    mainModal.classList.add('main-modal--opened');
-  } else {
-    mainModal.classList.add('main-modal--closed');
-    mainModal.classList.remove('main-modal--opened');
-  }
-})
-maimodalButton.addEventListener('click', function () {
-  if (mainModal.classList.contains('main-modal--opened')) {
-    mainModal.classList.remove('main-modal--opened');
-    mainModal.classList.add('main-modal--closed');
-  }
-  else {
-    mainModal.classList.add('main-modal--closed');
-    mainModal.classList.remove('main-modal--opened');
-  }
-});
-let aboutExtension = document.querySelector('.about__extension');
-let aboutExtensionButton = document.querySelector('.about__button');
-
-aboutExtensionButton.addEventListener('click', function () {
-  if (aboutExtension.classList.contains('about__extension--closed')) {
-    aboutExtension.classList.remove('about__extension--closed');
-    aboutExtension.classList.add('about__extension--opened');
-  } else {
-    aboutExtension.classList.add('about__extension--closed');
-    aboutExtension.classList.remove('about__extension--opened');
-  }
-});
-
-let aboutButton = document.querySelector('.about__button');
-aboutButton.addEventListener('click', function () {
-  (aboutButton.innerHTML === 'Подробнее') ? aboutButton.innerHTML = 'Свернуть' : aboutButton.innerHTML = 'Подробнее';
-});
-
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-  animationTime = 300,
-  framesCount = 20;
-
-anchors.forEach(function (item) {
-  // каждому якорю присваиваем обработчик события
-  item.addEventListener('click', function (e) {
-    // убираем стандартное поведение
-    e.preventDefault();
-
-    // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-
-    // запускаем интервал, в котором
-    let scroller = setInterval(function () {
-      // считаем на сколько скроллить за 1 такт
-      let scrollBy = coordY / framesCount;
-
-      // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
-      // и дно страницы не достигнуто
-      if (scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-        // то скроллим на к-во пикселей, которое соответствует одному такту
-        window.scrollBy(0, scrollBy);
-      } else {
-        // иначе добираемся до элемента и выходим из интервала
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-      // время интервала равняется частному от времени анимации и к-ва кадров
-    }, animationTime / framesCount);
-  });
-});
-
 const accordions = document.querySelectorAll(".accordion");
 const mobileQuery = "(max-width: 767px)";
 const mobileMedia = window.matchMedia(mobileQuery);
@@ -178,8 +73,8 @@ function closePreviousAccordion(index) {
   };
 }
 
-const hideSection = document.querySelector(".hide-section");
-const trigger = hideSection.querySelector(".hide-section__trigger");
+const hideSection = document.querySelector(".about__right-side");
+const trigger = hideSection.querySelector(".about__trigger");
 const hiddenElement = hideSection.querySelector("[data-hide]");
 const trimmedElement = hideSection.querySelector("[data-trim]");
 const savedTrimedText = trimmedElement.innerText;
